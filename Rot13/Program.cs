bd68c6833c154243030ff5d2a87eccc9f0f2a86d
Sinan Altaii <sinan.altaii@live.se>
2021-02-15 23:29:02 +0100

    progress

---
+++ Rot13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Rot13
+{
+	class Program
+	{
+		private static char[] Alphabets = new[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+		private const string TestExpected = "grfg";
+		private const string Test = "test";
+		//private const string SwedishExpected = "Övning";
+		static void Main(string[] args)
+		{
+			var testActual = Rot13(Test);
+			//var swedishActual = Rot13(SwedishExpected)
+			var isPassed = testActual == TestExpected;
+			Console.WriteLine($"Test actual: {testActual}. {nameof(TestExpected)}: {TestExpected}");
+		}
+
+		public static string Rot13(string message)
+		{
+			
+		}
+	}
+}
